d150e6030ef45b86989ffc2eef993eb8347bf41d
EdLeonard <leonardet19@student.cbhs.school.nz>
2021-04-09 11:00:30 +1200

    'worked on view more button, changed font, addd some info'

--- button.js
+++ button.js
@@ -2,10 +2,26 @@ function swapStyleSheet(sheet) {
     document.getElementById("pagestyle").setAttribute("href", sheet);  
 }
 
-function initate() {
+function viewmore1() {
     var style1 = document.getElementById("first-button-more");
-     var style1 = document.getElementById("first-button-less");
 
     style1.onclick = swapStyleSheet("internalviewmore.css");
-    style1.onclick = swapStyleSheet("internalstyle.css");
+} 
+
+function viewless1() {
+        var style2 = document.getElementById("first-button-less");
+
+        style2.onclick = swapStyleSheet("internalstyle.css");
+}
+
+function viewmore2() {
+    var style1 = document.getElementById("second-button-more");
+
+    style1.onclick = swapStyleSheet("internalviewmore.css");
+} 
+
+function viewless2() {
+        var style2 = document.getElementById("second-button-less");
+
+        style2.onclick = swapStyleSheet("internalstyle.css");
 }
