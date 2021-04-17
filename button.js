3823ef3c8630f490cbae3f6c065c74bfec9b614f
EdLeonard <leonardet19@student.cbhs.school.nz>
2021-04-17 23:13:30 +1200

    'finished information for main page, switched up header and other stuff'

--- button.js
+++ button.js
@@ -13,15 +13,3 @@ function viewless1() {
 
         style2.onclick = swapStyleSheet("internalstyle.css");
 }
-
-function viewmore2() {
-    var style1 = document.getElementById("second-button-more");
-
-    style1.onclick = swapStyleSheet("internalviewmore.css");
-} 
-
-function viewless2() {
-        var style2 = document.getElementById("second-button-less");
-
-        style2.onclick = swapStyleSheet("internalstyle.css");
-}
