3c18d1e38ad25d17e0c76b3686bd0e8a54467e7b
EdLeonard <leonardet19@student.cbhs.school.nz>
2021-04-20 22:58:52 +1200

    'started working on second page'

--- button.js
+++ button.js
@@ -1,15 +1,15 @@
 function swapStyleSheet(sheet) {
-    document.getElementById("pagestyle").setAttribute("href", sheet);  
+  document.getElementById("pagestyle").setAttribute("href", sheet);
 }
 
 function viewmore1() {
-    var style1 = document.getElementById("first-button-more");
+  var style1 = document.getElementById("first-button-more");
 
-    style1.onclick = swapStyleSheet("internalviewmore.css");
-} 
+  style1.onclick = swapStyleSheet("internalviewmore.css");
+}
 
 function viewless1() {
-        var style2 = document.getElementById("first-button-less");
+  var style2 = document.getElementById("first-button-less");
 
-        style2.onclick = swapStyleSheet("internalstyle.css");
+  style2.onclick = swapStyleSheet("internalstyle.css");
 }
