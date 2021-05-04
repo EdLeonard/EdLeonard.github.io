d3af12ff21a010b36e7bcaab10ad710d29ee441c
EdLeonard <leonardet19@student.cbhs.school.nz>
2021-05-04 13:00:08 +1200

    'tried to fix up messy css files, kinda ruined it just seeing if its working'

--- button.js
+++ button.js
@@ -5,11 +5,11 @@ function swapStyleSheet(sheet) {
 function viewmore1() {
   var style1 = document.getElementById("first-button-more");
 
-  style1.onclick = swapStyleSheet("internalviewmore.css");
+  style1.onclick = swapStyleSheet("indexviewmore.css");
 }
 
 function viewless1() {
   var style2 = document.getElementById("first-button-less");
 
-  style2.onclick = swapStyleSheet("indexstyle.css");
+  style2.onclick = swapStyleSheet("indexstyle.css", "styles.css");
 }
