345638a7ebcc94effb0aae576a9604395aecc4cd
EdLeonard <leonardet19@student.cbhs.school.nz>
2021-05-10 11:22:52 +1200

    'worked on secondary pages'

--- button.js
+++ button.js
@@ -2,13 +2,13 @@ function swapStyleSheet(sheet) {
   document.getElementById("pagestyle").setAttribute("href", sheet);
 }
 
-function viewmore1() {
+function viewmore() {
   var style1 = document.getElementById("first-button-more");
 
-  style1.onclick = swapStyleSheet("indexviewmore.css");
+  style1.onclick = swapStyleSheet("indexviewmore.css", "styles.css");
 }
 
-function viewless1() {
+function viewless() {
   var style2 = document.getElementById("first-button-less");
 
   style2.onclick = swapStyleSheet("indexstyle.css", "styles.css");
